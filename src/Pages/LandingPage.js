ddbc34322bfd473eeb8e3ee4697f1e8f35918a4a
Sonia Irawan <soniairawan99@gmail.com>
2021-12-03 12:16:48 +0700

    component button dan header

---
+++ src/Pages/LandingPage.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react'
+
+
+import Header from 'parts/Header'
+
+export default class LandingPage extends Component{
+    render() {
+        console.log(this.props)
+        return (
+            <>
+               <Header {...this.props}></Header> 
+            </>
+        )
+    }
+}
